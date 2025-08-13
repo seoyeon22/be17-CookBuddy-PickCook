d7da8b4dd8052bedb80f440a029180b89023b97b
seoyoen22 <hsy2709@gmail.com>
2025-08-13 12:34:26 +0900

    initial commit

---
+++ src/main/java/org/example/be17pickcook/Be17PickcookApplication.java
@@ -0,0 +1,13 @@
+package org.example.be17pickcook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Be17PickcookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Be17PickcookApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/be17pickcook/Be17PickcookApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.be17pickcook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Be17PickcookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
