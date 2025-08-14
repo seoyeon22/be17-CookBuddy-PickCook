366b547175931ebde3d7b05bcbf33b5a5d83b055
seoyoen22 <hsy2709@gmail.com>
2025-08-14 17:40:32 +0900

    좋아요 및 스크랩 수 조회

--- src/main/java/org/example/be17pickcook/domain/community/service/PostService.java
+++ src/main/java/org/example/be17pickcook/domain/community/service/PostService.java
@@ -4,6 +4,10 @@ import lombok.RequiredArgsConstructor;
 import org.example.be17pickcook.domain.community.model.Post;
 import org.example.be17pickcook.domain.community.model.PostDto;
 import org.example.be17pickcook.domain.community.repository.PostRepository;
+import org.example.be17pickcook.domain.likes.model.LikeTargetType;
+import org.example.be17pickcook.domain.likes.service.LikesService;
+import org.example.be17pickcook.domain.scrap.model.ScrapTargetType;
+import org.example.be17pickcook.domain.scrap.service.ScrapService;
 import org.example.be17pickcook.domain.user.model.User;
 import org.example.be17pickcook.domain.user.model.UserDto;
 import org.example.be17pickcook.domain.user.repository.UserRepository;
@@ -18,20 +22,28 @@ public class PostService {
 
     private final PostRepository postRepository;
     private final UserRepository userRepository;
+    private final LikesService likesService;
+    private final ScrapService scrapService;
 
     // 전체 게시글 조회
     public List<PostDto.Response> getAllPosts() {
         List<Post> postList = postRepository.findAll();
         return postList.stream()
-                .map(PostDto.Response::toResponse)
+                .map(post -> {
+                    int likesCount = likesService.getLikesCount(LikeTargetType.COMMUNITY, post.getId());
+                    int scrapsCount = scrapService.getScrapCount(ScrapTargetType.COMMUNITY, post.getId());
+                    return PostDto.Response.toResponse(post, likesCount, scrapsCount);
+                })
                 .collect(Collectors.toList());
     }
 
     // 게시글 상세 조회
     public PostDto.Response getPostById(Long id) {
-        Post entity = postRepository.findById(id)
+        Post post = postRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("게시글이 존재하지 않습니다."));
-        return PostDto.Response.toResponse(entity);
+        int likesCount = likesService.getLikesCount(LikeTargetType.COMMUNITY, post.getId());
+        int scrapsCount = scrapService.getScrapCount(ScrapTargetType.COMMUNITY, post.getId());
+        return PostDto.Response.toResponse(post, likesCount, scrapsCount);
     }
 
     // 게시글 작성
@@ -44,7 +56,9 @@ public class PostService {
         Post post = dto.toEntity(user); // toEntity에서 User 객체 받도록 수정
 
         Post saved = postRepository.save(post);
-        return PostDto.Response.toResponse(saved);
+        int likesCount = likesService.getLikesCount(LikeTargetType.COMMUNITY, post.getId());
+        int scrapsCount = scrapService.getScrapCount(ScrapTargetType.COMMUNITY, post.getId());
+        return PostDto.Response.toResponse(saved, likesCount, scrapsCount);
     }
 
 }
