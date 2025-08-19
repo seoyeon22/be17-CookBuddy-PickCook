a4f344e681f8b6812df6316a267488ba84fe2ba9
thay123028 <thay123028@naver.com>
2025-08-19 15:39:57 +0900

    레시피 엔티티에 레시피 소요시간, 난이도 정보 추가

--- src/main/java/org/example/be17pickcook/domain/recipe/model/Recipe.java
+++ src/main/java/org/example/be17pickcook/domain/recipe/model/Recipe.java
@@ -24,10 +24,12 @@ public class Recipe extends BaseEntity {
     private String description;
     private String cooking_method;
     private String category;
+    private String time_taken;
+    private String difficulty_level;
     private String serving_size;
     private String hashtags;
-    private String image_small_url;
-    private String image_large_url;
+    private String image_small_url; // 필수 아님
+    private String image_large_url; // 필수
     private String tip;
 
 
